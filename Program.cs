1e7c83139184a135d0433abd76136cf44e362183
Pedro Martinelli <p.martinellinunes@gmail.com>
2023-08-18 12:07:57 -0300

    criando o menu e um editor de texto

--- Program.cs
+++ Program.cs
@@ -1,9 +1,49 @@
-﻿namespace TextEditor;
+﻿using Console = System.Console;
+
+namespace TextEditor;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Menu();
     }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("O que deseja realizar?");
+        Console.WriteLine("1 - Abrir arquivo");
+        Console.WriteLine("2 - Criar novo arquivo");
+        Console.WriteLine("0 - Sair");
+
+        short option = short.Parse(Console.ReadLine()!);
+
+        switch (option)
+        {
+            case 1: Open(); break;
+            case 2: Edit(); break;
+            case 0: System.Environment.Exit(0); break;
+            default: Menu(); break;
+        }
+    }
+
+    static void Open() { }
+
+    static void Edit()
+    {
+        Console.Clear();
+        Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+        Console.WriteLine("------------------------");
+        string text = "";
+
+        while (Console.ReadKey().Key != ConsoleKey.Escape)
+        {
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+
+        }
+        Console.Write(text);
+    }
+
 }
